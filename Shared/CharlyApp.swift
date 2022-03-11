9aae38ffd563e605882abb8d1822bd117134df81
Dan Hart <dhart94@gmail.com>
2022-03-11 15:43:36 -0500

    Initial Commit

---
+++ Shared/CharlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharlyApp.swift
+//  Shared
+//
+//  Created by Dan Hart on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CharlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
